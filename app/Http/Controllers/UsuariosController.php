841c5a1e06e444fa9df14e3c9d68cd497b2fec73
Juan Pablo Guzmán Rosero <jguzman@udenar.edu.co>
2023-05-28 16:21:52 -0500

    Controlador Usuarios

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuariosController extends Controller
+{
+    public function index()
+    {
+        return view('usuarios.index');
+    }
+
+    public function create()
+    {
+        return view('usuarios.create');
+    }
+
+    public function store(Request $request)
+    {
+        return redirect()->route('usuarios.index');
+    }
+}
