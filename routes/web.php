ad8aa74da929526974dc6bd481266643fd57108f
Juan Pablo Guzmán Rosero <jguzman@udenar.edu.co>
2023-05-28 20:48:50 -0500

    Readición de las rutas para las vistas usuarios.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', [UsuariosController::class, 'index'])->name('usuarios.index');
+Route::get('/usuarios/crear', [UsuariosController::class, 'create'])->name('usuarios.create');
